fbf08f7717816d2ebcdcc5f37c2928ffd2e43c70
opilane <opilane@HP-CZC6297B93>
2019-06-04 12:02:02 +0300

    Add project files.

---
+++ Autotookoda1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Autotookoda1.MvcApplication" Language="C#" %>
---
+++ Autotookoda1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Autotookoda1.Startup))]
+namespace Autotookoda1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
